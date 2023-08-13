bd6191407b728282e8a6a28f9cbd674bb3d7e190
tlskals <chlwlals8708@gmail.com>
2023-08-13 20:33:56 +0900

    first commit!

---
+++ logging-solution/src/main/java/pptware/loggingsolution/LoggingSolutionApplication.java
@@ -0,0 +1,13 @@
+package pptware.loggingsolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingSolutionApplication.class, args);
+	}
+
+}
---
+++ logging-solution/src/test/java/pptware/loggingsolution/LoggingSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package pptware.loggingsolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
